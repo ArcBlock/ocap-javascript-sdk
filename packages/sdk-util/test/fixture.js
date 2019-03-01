86e61faf199e7378ed5c775ae0f8a63e267b94e4
wangshijun <wangshijun2010@gmail.com>
2019-03-01 11:20:03 +0800

    better support to format arbitary arguments (#84)

--- packages/sdk-util/test/fixture.js
+++ packages/sdk-util/test/fixture.js
@@ -1,3 +1,130 @@
+const extractedArgSpecs = {
+  addressFilter: {
+    defaultValue: null,
+    description: '',
+    fields: {
+      direction: {
+        defaultValue: null,
+        description: null,
+        name: 'direction',
+        type: { kind: 'ENUM', name: 'Direction', ofType: null },
+      },
+      receiver: {
+        defaultValue: null,
+        description: null,
+        name: 'receiver',
+        type: { kind: 'SCALAR', name: 'String', ofType: null },
+      },
+      sender: {
+        defaultValue: null,
+        description: null,
+        name: 'sender',
+        type: { kind: 'SCALAR', name: 'String', ofType: null },
+      },
+    },
+    name: 'addressFilter',
+    type: { kind: 'INPUT_OBJECT', name: 'AddressFilter', ofType: null },
+  },
+  paging: {
+    defaultValue: null,
+    description: '',
+    fields: {
+      cursor: {
+        defaultValue: null,
+        description: null,
+        name: 'cursor',
+        type: { kind: 'SCALAR', name: 'String', ofType: null },
+      },
+      order: {
+        defaultValue: null,
+        description: null,
+        name: 'order',
+        type: {
+          kind: 'LIST',
+          name: null,
+          ofType: {
+            fields: {
+              field: {
+                defaultValue: null,
+                description: null,
+                name: 'field',
+                type: { kind: 'SCALAR', name: 'String', ofType: null },
+              },
+              type: {
+                defaultValue: null,
+                description: null,
+                name: 'type',
+                type: { kind: 'SCALAR', name: 'String', ofType: null },
+              },
+            },
+            kind: 'INPUT_OBJECT',
+            name: 'PageOrder',
+            ofType: null,
+          },
+        },
+      },
+      size: {
+        defaultValue: null,
+        description: null,
+        name: 'size',
+        type: { kind: 'SCALAR', name: 'Int', ofType: null },
+      },
+    },
+    name: 'paging',
+    type: { kind: 'INPUT_OBJECT', name: 'PageInput', ofType: null },
+  },
+  timeFilter: {
+    defaultValue: null,
+    description: '',
+    fields: {
+      endDateTime: {
+        defaultValue: null,
+        description: null,
+        name: 'endDateTime',
+        type: { kind: 'SCALAR', name: 'String', ofType: null },
+      },
+      startDateTime: {
+        defaultValue: null,
+        description: null,
+        name: 'startDateTime',
+        type: { kind: 'SCALAR', name: 'String', ofType: null },
+      },
+    },
+    name: 'timeFilter',
+    type: { kind: 'INPUT_OBJECT', name: 'TimeFilter', ofType: null },
+  },
+  token: {
+    defaultValue: null,
+    description: '',
+    name: 'token',
+    type: { kind: 'SCALAR', name: 'String', ofType: null },
+  },
+  typeFilter: {
+    defaultValue: null,
+    description: '',
+    fields: {
+      types: {
+        defaultValue: null,
+        description: null,
+        name: 'types',
+        type: {
+          kind: 'LIST',
+          name: null,
+          ofType: { kind: 'SCALAR', name: 'String', ofType: null },
+        },
+      },
+    },
+    name: 'typeFilter',
+    type: { kind: 'INPUT_OBJECT', name: 'TypeFilter', ofType: null },
+  },
+  types: {
+    defaultValue: null,
+    description: null,
+    name: 'types',
+    type: { kind: 'LIST', name: null, ofType: { kind: 'SCALAR', name: 'String', ofType: null } },
+  },
+};
+
 const mutationCreateWallet = `mutation {
   createWallet(moniker: "wangshijun", passphrase: "1234556") {
     code
@@ -18,7 +145,7 @@ const mutationCreateWallet = `mutation {
 `;
 
 const queryListTransactions = `{
-  listTransactions(paging: {size: 1}, typeFilter: {types: ["AccountMigrate", "Transfer"]}) {
+  listTransactions(paging: {size: 1}, typeFilter: {types: ["AccountMigrate", "Transfer"]}, addressFilter: {sender: "123", receiver: "123", direction: UNION}) {
     code
     page {
       cursor
@@ -113,6 +240,7 @@ const queryListTransactions = `{
             }
           }
           ... on CreateAssetTx {
+            expiredAt
             moniker
             readonly
             data {
@@ -151,6 +279,7 @@ const queryListTransactions = `{
 `;
 
 module.exports = {
+  extractedArgSpecs,
   mutationCreateWallet,
   queryListTransactions,
 };
